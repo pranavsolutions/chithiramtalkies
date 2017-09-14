a4fb2d5f72a6793844d303e26266bbdd916cbd40
pranavsolutions <praveenrulz@gmail.com>
2017-09-13 20:45:30 -0400

    Initial setup

---
+++ assets/portfolio.js
@@ -0,0 +1,37 @@
+            // init Isotope
+            var $grid = $('.portfolio').isotope({
+            itemSelector: '.item',
+            layoutMode: 'fitRows',
+            getSortData: {
+            name: '.name',
+            symbol: '.symbol',
+            number: '.number parseInt',
+            category: '[data-category]',
+            weight: function( itemElem ) {
+                  var weight = $( itemElem ).find('.weight').text();
+                  return parseFloat( weight.replace( /[\(\)]/g, '') );
+            }
+            }
+            });
+
+            // filter functions
+            var filterFns = {
+            // show if number is greater than 50
+            numberGreaterThan50: function() {
+            var number = $(this).find('.number').text();
+            return parseInt( number, 10 ) > 50;
+            },
+            // show if name ends with -ium
+            ium: function() {
+            var name = $(this).find('.name').text();
+            return name.match( /ium$/ );
+            }
+            };
+
+            // bind filter button click
+            $('.filters').on( 'click', 'button', function() {
+            var filterValue = $( this ).attr('data-filter');
+            // use filterFn if matches value
+            filterValue = filterFns[ filterValue ] || filterValue;
+            $grid.isotope({ filter: filterValue });
+            });
